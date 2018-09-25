f4858ecb4a93ffde5b998a7d4ae03c231a04fea3
Nimo Chu <nimo.jser@gmail.com>
2018-09-25 11:55:24 +0800

    match rType

--- test/ajax.test.js
+++ test/ajax.test.js
@@ -10,19 +10,76 @@ const app = createApp(function (mock) {
             }
         }
     })
+    mock.url('/login', {
+        type: 'put'
+    })
+    mock.url('/news', {
+        type: 'get',
+        data: {
+            pass: {
+                lists: [
+                    '瑞典电视台回应称“表达整体意思出现缺失”并未道歉',
+                    '这个商品进了贸易战清单 俄罗斯愿助中国一臂之力',
+                    '中国对外贸易实现历史性跨越 40年进口增长664倍'
+                ]
+            },
+            empty:{
+                list: []
+            },
+            $empty: {
+                match: {
+                    search: {
+                        pattern: 'abc'
+                    }
+                }
+            }
+        }
+    })
 })
 
 describe('ajax.js', function() {
     it('should return pass data', function(done) {
         request(app)
-        .post('/login?some=abc')
-        .send('a=1')
+        .post('/login')
         .expect(200)
         .then(res => {
             Object.keys(res.body).should.eql(['type', 'balance'])
             res.body.type.should.equal('pass')
             res.body.balance.should.above(99)
             res.body.balance.should.below(201)
+            done()
+        })
+    })
+    it('should return pass fail POST(_=fail)', function(done) {
+        request(app)
+        .post('/login')
+        .send('_=fail')
+        .expect(200)
+        .then(res => {
+            Object.keys(res.body).should.eql(['type', 'msg'])
+            res.body.type.should.equal('fail')
+            done()
+        })
+    })
+    it('should return pass fail PUT(_=fail)', function(done) {
+        request(app)
+        .put('/login')
+        .send('_=fail')
+        .expect(200)
+        .then(res => {
+            Object.keys(res.body).should.eql(['type', 'msg'])
+            res.body.type.should.equal('fail')
+            done()
+        })
+    })
+    it('should return empty GET(search_=abc)', function(done) {
+        request(app)
+        .get('/news?search=abc')
+        .expect(200)
+        .then(res => {
+            Object.keys(res.body).should.eql(['type', 'list'])
+            res.body.type.should.equal('empty')
+            res.body.list.length.should.equal(0)
             done()
         })
     })
